4ceff66c12a1195ecbf67775efc297a2ae51d494
Vishnuprasanth-mediwave <vishnu_prasanth@mindwaveventures.com>
2023-10-06 15:44:57 +0530

    first commit

--- app/src/App.js
+++ app/src/App.js
@@ -1,91 +1,86 @@
-import { Formik, Form, Field } from "formik";
+import React, { useState } from "react";
 import Input from "./Components/input";
 import Button from "./Components/button";
-import "./App.css";
-import * as Yup from "yup";
-import { useState } from "react";
 import Card from "./Components/card";
 
-const validationSchema = Yup.object().shape({
-  title: Yup.string().required("Title is required"),
-  description: Yup.string()
-    .required("Description is required")
-    .min(5, "Description should be at least 5 characters"),
-  ratings: Yup.number()
-    .required("Ratings is required")
-    .max(10, "Rating cannot be greater than 10"),
-  url: Yup.string().url("Enter a valid URL").required("URL is required"),
-});
-
 function App() {
-  const initialValues = {
+  const [formData, setFormData] = useState([{
     title: "",
     description: "",
     ratings: "",
     url: "",
+  }]);
+  const [cardData, setCardData] = useState([]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
   };
-  const [carddata, setCarddata] = useState([]);
-  const handleSubmit = (values, { resetForm }) => {
-    console.log(values);
-    setCarddata([...carddata, values]);
-    resetForm();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setCardData([...cardData, formData]);
+    setFormData({
+      title: "",
+      description: "",
+      ratings: "",
+      url: "",
+    });
   };
 
   return (
     <div>
-      <Formik
-        initialValues={initialValues}
-        onSubmit={handleSubmit}
-        validationSchema={validationSchema}
-      >
-        {({ touched, errors }) => (
-          <Form>
-            <div>
-              <label htmlFor="title">Title</label>
-              <Field type="text" id="title" name="title" placeholder="Title" />
-              {touched.title && errors.title && (
-                <div className="error">{errors.title}</div>
-              )}
-            </div>
-            <div>
-              <label htmlFor="description">Description</label>
-              <Field
-                type="text"
-                id="description"
-                name="description"
-                placeholder="Description"
-              />
-              {touched.description && errors.description && (
-                <div className="error">{errors.description}</div>
-              )}
-            </div>
-            <div>
-              <label htmlFor="ratings">Ratings</label>
-              <Field
-                type="number"
-                id="ratings"
-                name="ratings"
-                placeholder="Ratings"
-              />
-              {touched.ratings && errors.ratings && (
-                <div className="error">{errors.ratings}</div>
-              )}
-            </div>
-            <div>
-              <label htmlFor="url">URL</label>
-              <Field type="url" id="url" name="url" placeholder="URL" />
-              {touched.url && errors.url && (
-                <div className="error">{errors.url}</div>
-              )}
-            </div>
-            <Button type="submit" name="Submit" />
-          </Form>
-        )}
-      </Formik>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="title">Title</label>
+          <Input
+            type="text"
+            id="title"
+            name="title"
+            placeholder="Title"
+            value={formData.title}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="description">Description</label>
+          <Input
+            type="text"
+            id="description"
+            name="description"
+            placeholder="Description"
+            value={formData.description}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="ratings">Ratings</label>
+          <Input
+            type="number"
+            id="ratings"
+            name="ratings"
+            placeholder="Ratings"
+            value={formData.ratings}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="url">URL</label>
+          <Input
+            type="url"
+            id="url"
+            name="url"
+            placeholder="URL"
+            value={formData.url}
+            onChange={handleChange}
+          />
+        </div>
+        <Button type="submit" name="Submit" />
+      </form>
       <div className="card">
-        {carddata &&
-          carddata.map((item) => {
-            return <Card values={item} />;
+        {cardData &&
+          cardData.map((item, index) => {
+            return <Card key={index} values={item} />;
           })}
       </div>
     </div>
